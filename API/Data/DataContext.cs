bf121bf0007b58e13b18195228c00681eefc135a
joaomteixeira11 <joao.m.teixeira@aitec.pt>
2024-07-09 10:40:24 +0100

    datingApp

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; } // Uses Id from AppUsser as the primary key
+}
