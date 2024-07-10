bce8d2c2c88fe9e681bad1836907c91a480e9971
joaomteixeira11 <joao.m.teixeira@aitec.pt>
2024-07-10 09:37:56 +0100

    token services

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+// Logic for creating a JSON Web Token (JWT) that we ca then use to return to the browser
+// for that user when they do log in or register
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot acess tokenKey from appsettings");
+
+        if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.UserName)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+
+    }
+}
